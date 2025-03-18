5346e91e3fca3c1843349313b31754e3a3790118
Lemons <jacob.lemons@colorado.edu>
2025-03-18 17:35:55 -0600

    ica10 and phone num demo

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,8 @@
+let box = document.querySelector(".div-box");
+box.addEventListener("click", changeTheme);
+
+function changeTheme() {
+  body.style = "background-color: black;";
+  box.style =
+    "background-color: border-color: rgb(24, 224, 24); border-color: rgb(24, 224, 24); right: 0px;";
+}
