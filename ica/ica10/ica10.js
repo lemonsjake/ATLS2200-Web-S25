d11c725284ad6f4efb547964ce8bdef4f33fb15e
Lemons <jacob.lemons@colorado.edu>
2025-03-18 21:16:32 -0600

    Completed ica10
    Finished ica10

--- ica/ica10/ica10.js
+++ ica/ica10/ica10.js
@@ -1,8 +1,38 @@
-let box = document.querySelector(".div-box");
-box.addEventListener("click", changeTheme);
+let btnOne = document.querySelector("#findOut");
+let btnTwo = document.querySelector("#elmTwo");
 
-function changeTheme() {
-  body.style = "background-color: black;";
-  box.style =
-    "background-color: border-color: rgb(24, 224, 24); border-color: rgb(24, 224, 24); right: 0px;";
+btnOne.addEventListener("click", nowSad);
+btnTwo.addEventListener("click", evenSadder);
+
+console.log("test0");
+
+function nowSad() {
+  console.log("test11");
+  let sad = document.querySelector("#title");
+  sad.textContent = "sadness...";
+  console.log("test12");
+  btnOne.style = "display: none;";
 }
+
+function evenSadder() {
+  console.log("test2");
+  btnTwo.style = "padding: 3em; font-weight: 100;";
+}
+
+// let frame = document.querySelector('.bgFrame');
+// let baseBox = document.querySelector('.centerBox');
+// let baseBtn = document.querySelector('#browns');
+// let boxBtn = document.querySelector('#greens');
+
+// baseBtn.addEventListener('click', changeBrowns);
+// boxBtn.addEventListener('click', changeGreens);
+
+// function changeBrowns() {
+//   frame.style = "background-color: var(--cream-color); color: var(--brown-color);"
+//   baseBox.style = "background-color: var(--brown-color); color: var(--cream-color);"
+// }
+
+// function changeGreens() {
+//   baseBtn.style = "background-color: var(-frog--color); color: var(--forest-color); border-color: var(--frog-color);"
+//   boxBtn.style = "background-color: var(-frog--color); color: var(--forest-color); border-color: var(--frog-color);"
+// }
