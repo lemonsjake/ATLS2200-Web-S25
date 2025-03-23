5c553921dfad09eed09b1811743bb7aba1c711dd
Lemons <jacob.lemons@colorado.edu>
2025-03-23 16:46:54 -0600

    Completed wa10
    Finished Weekly Assignment 10: A Silly Story

--- wa/wa10/wa10.js
+++ wa/wa10/wa10.js
@@ -12,26 +12,53 @@ function randomValueFromArray(array) {
 
 // RAW TEXT STRINGS
 let storyText =
-  "It was a brisk 39 fahrenheit rainy night outside the :inserty:, as :insertx: sat atop the building in the rain. Suddenly, :insertz: and :insertz: showed up and sent :insertx: slamming into the ground below with 1000 pounds of force. :insertx: won and went inside after staring up into the sky for a few moments longer, contemplating if their will ever be a day when the villians stop. The Watcher watched it all and couldn't help but wonder the same question.";
-const insertX = [
+  'It was a brisk 41 fahrenheit rainy night outside the :insertl:, as :inserth1: sat atop the building in the rain. Suddenly, :insertv1: and :insertv2: showed up and sent :inserth1: slamming into the ground below with 2000 pounds of force. :inserth2: surprised the villians from behind, joining the fight. :inserth1: rejoined the battle fighting alongside :inserth2:. The two heros won then went inside after staring up into the sky together and contemplating the question posed by :inserth1:, "will there ever be a day when the villians stop?" Dinkysquatch The Watcher watched it all from beyond the Cosmos, unable to help but ponder the posed question.';
+const insertL = [
+  "Baxter Building",
+  "Avengers Tower",
+  "X-Mansion",
+  "Avengers Mansion",
+  "Sanctum Sanctorum",
+  "Oscorp Tower",
+  "Stark Industries Tower",
+];
+const insertH = [
   "Spider-Man",
+  "She-Hulk",
   "Iron-Man",
   "Kate Bishop",
-  "Ms. Marvel",
-  "Sue Storm",
-  "Wanda Maximoff",
   "Deadpool",
-  "Thor"
+  "Ms. Marvel",
+  "Wolverine",
+  "Sue Storm",
+  "Daredevil",
+  "Wanda Maximoff",
+  "Thor",
+  "Jean Grey",
+  "Doctor Strange",
+  "Spider-Women",
+  "Miles Morales",
 ];
-const insertY = ["Baxter Building", "Avengers Tower", "X-Mansion"];
-const insertZ = [
-  "The Green Goblin",
-  "The Shocker",
+const insertV = [
+  "Green Goblin",
+  "Shocker",
+  "Madame Masque",
   "an army of DoomBots",
   "a Sentinel",
   "Carnage",
-  "The Vulture",
+  "Vulture",
+  "Titania",
   "Kingpin",
+  "Viper",
+  "Doctor Octopus",
+  "Electro",
+  "Tombstone",
+  "Sin",
+  "Rhino",
+  "Kraven the Hunter",
+  "Lady Deathstrike",
+  "Hammerhead",
+  "Mister Negative",
 ];
 
 // EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
@@ -39,30 +66,34 @@ randomize.addEventListener("click", result);
 
 function result() {
   let newStory = storyText;
-  let yItem = randomValueFromArray(insertY);
-  let xItem = randomValueFromArray(insertX);
-  let zItem = randomValueFromArray(insertZ);
-  let zItemZ = randomValueFromArray(insertZ);
+  let loctn = randomValueFromArray(insertL);
+  let hero1 = randomValueFromArray(insertH);
+  let viln1 = randomValueFromArray(insertV);
+  let viln2 = randomValueFromArray(insertV);
+  let hero2 = randomValueFromArray(insertH);
 
-  newStory = newStory.replace(":inserty:", yItem);
-  newStory = newStory.replace(":insertx:", xItem);
-  newStory = newStory.replace(":insertz:", zItem);
-  newStory = newStory.replace(":insertz:", zItemZ);
-  newStory = newStory.replace(":insertx:", xItem);
-  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":insertl:", loctn);
+  newStory = newStory.replace(":inserth1:", hero1);
+  newStory = newStory.replace(":insertv1:", viln1);
+  newStory = newStory.replace(":insertv2:", viln2);
+  newStory = newStory.replace(":inserth1:", hero1);
+  newStory = newStory.replace(":inserth2:", hero2);
+  newStory = newStory.replace(":inserth1:", hero1);
+  newStory = newStory.replace(":inserth2:", hero2);
+  newStory = newStory.replace(":inserth1:", hero1);
 
-  if (customName.value !== "The Watcher") {
+  if (customName.value !== "Dinkysquatch") {
     const name = customName.value;
 
-    newStory = newStory.replace("The Watcher", name);
+    newStory = newStory.replace("Dinkysquatch", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(1000 / 14)} stone`;
-    const temperature = `${Math.round((18 - 32) * (5 / 9))} centigrade`;
+    const weight = `${Math.round(2000 / 14)} stone`;
+    const temperature = `${Math.round((41 - 32) * (5 / 9))} centigrade`;
 
-    newStory = newStory.replace("1000 pounds", weight);
-    newStory = newStory.replace("39 fahrenheit", temperature);
+    newStory = newStory.replace("2000 pounds", weight);
+    newStory = newStory.replace("41 fahrenheit", temperature);
   }
 
   story.textContent = newStory;
