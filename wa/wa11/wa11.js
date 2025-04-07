b537d96a57b3ad5a8cf1edb9a1dec6783fb11305
Lemons <jacob.lemons@colorado.edu>
2025-04-06 18:51:40 -0600

    Completed WA11
    Finished Weekly Assignment 11 - An Image Carousel

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -4,6 +4,10 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
+/* NOT REQUIRED -- PERSONAL ADDITION */
+const hd1 = document.querySelector("h1");
+// const bdy = document.querySelector("body");
+
 /* Declaring the array of image filenames */
 const imageFilenames = [
   "pic1.jpg",
@@ -32,13 +36,36 @@ for (pic in imageFilenames) {
   thumbBar.appendChild(newImage);
 
   /* Click event listener sets displayed image to clicked thumbnail */
-  newImage.addEventListener(
-    "click",
-    () => displayedImage.setAttribute("src", imgSrc),
-    displayedImage.setAttribute("alt", imgAlt)
-  );
+  newImage.addEventListener("click", () => {
+    displayedImage.setAttribute("src", newImage.getAttribute("src"));
+    displayedImage.setAttribute("alt", newImage.getAttribute("alt"));
+  });
+  // HOW WOULD THIS WORK WITH JUST ADDING ONE EVENT LISTENER TO THE THUMBBAR ??
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", () => {
+  if (btn.getAttribute("class") === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
 
-// MAYBE CHANGE BACKGROUND COLOR WITH LIGHTEN/DARKEN
+    /* NOT REQUIRED -- PERSONAL ADDITION */
+    hd1.style.fontFamily = "Josefin Sans";
+    hd1.style.textShadow =
+      "skyblue 0 0 0.1rem, cornflowerblue 0 0 0.33rem, cornsilk 0 0 0.67rem, cornsilk 0 0 1rem";
+    hd1.style.color = "#1e1e1e";
+    // bdy.style.backgroundColor = "#1e1e1e";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+
+    /* NOT REQUIRED -- MY ADDITION */
+    hd1.style.fontFamily = "Quicksand";
+    hd1.style.textShadow =
+      "skyblue 0 0 0.33rem, royalblue 0 0 0.67rem, blue 0 0 1rem";
+    hd1.style.color = "white";
+    // bdy.style.backgroundColor = "white";
+  }
+});
