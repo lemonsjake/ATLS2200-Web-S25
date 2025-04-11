13cd53fb4f2597a048c07eea0505e78557b09d45
Lemons <jacob.lemons@colorado.edu>
2025-04-11 01:23:18 -0600

    cant get icon api to work

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -2,14 +2,18 @@
 let newQuoteBtn = document
   .querySelector("#js-new-quote")
   .addEventListener("click", getQuote);
-let saveBtn = document.querySelector("#js-save-icon");
+let chgIconBtn = document.querySelector("#js-change-icon").addEventListener("click", randomStyle);
+let saveIconBtn = document.querySelector("#js-save-icon");
 
-const answerText = document.querySelector("#js-answer-text");
+let iconImg = document.querySelector("#js-icon-img");
+// const answerText = document.querySelector("#js-answer-text");
 
-// let endPt = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
-let quoteEndPt = "https://stoic.tekloon.net/stoic-quote";
+// let endPt = "https://trivia.cyberwisp.com/getrandomchristmasquote";
+let quoteEndPt =
+  "https://api.codetabs.com/v1/proxy/?quest=stoic.tekloon.net/stoic-quote";
+// let quoteEndPt = "https://zenquotes.io?api=random";
 
-iconStyles = [
+let iconStyles = [
   "adventurer",
   "adventurer-neutral",
   "avataaars",
@@ -19,27 +23,28 @@ iconStyles = [
   "big-smile",
   "bottts",
   "bottts-neutral",
-  "croodles",
+  "croodles"
 ];
+
+// let randomStyleType = randomStyle();
 let iconEndPt = `https://api.dicebear.com/9.x/${randomStyle}/svg`;
 
 function randomStyle() {
-  return `iconStyles[Math.floor(Math.random() * iconStyles.length)]`;
+  return iconStyles[Math.floor(Math.random() * iconStyles.length)];
 }
-// get endPt;
 
-let current = {
-  question: "",
-  answer: "",
-};
+// let current = {
+//   quote: "",
+//   answer: "",
+// };
 
 async function getQuote() {
-  // console.log("getQuote called");
+  console.log("getQuote called");
 
-  answerText.textContent = "";
+  // answerText.textContent = "";
 
   try {
-    const response = await fetch(endPt);
+    const response = await fetch(quoteEndPt);
 
     if (!response.ok) {
       throw Error(response.statusText);
@@ -48,22 +53,93 @@ async function getQuote() {
     const json = await response.json();
     // console.log(json);
 
-    current.question = json.question;
-    current.answer = json.answer;
+    // current.quote = json.quote;
+    // current.answer = json.answer;
 
-    displayQuote(current);
+    let quoteItem = json.data.quote;
+    console.log(quoteItem);
+    let quoteAuthor = json.data.author;
+    console.log(quoteAuthor);
+
+    // let fullQuote = `${quoteItem} ${quoteAuthor}`;
+    // console.log(fullQuote);
+
+    // if (quoteItem.charAt(quoteItem.length - 1) === "@") {
+    //     console.log("Present @ character found. Removing now.");
+    //   quoteItem = quoteItem.substring(0, quoteItem.length - 2);
+    // }
+
+    displayQuote(quoteItem, quoteAuthor);
+    displayIcon();
+    // displayQuote(current);
   } catch (err) {
     console.log(err);
-    alert("FAIL");
+    alert("FAIL QUOTE");
   }
 }
 
-function displayQuote(curr) {
-  const questionText = document.querySelector("#js-question-text");
-  questionText.textContent = curr.question;
-  answerBtn.addEventListener("click", () => {
-    answerText.textContent = curr.answer;
-  });
+async function getIcon() {
+  console.log("getIcon called");
+
+  // answerText.textContent = "";
+
+  try {
+    const response = await fetch(iconEndPt);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+    // console.log(json);
+
+    iconImg.setAttribute("src", iconEndPt);
+
+    // current.quote = json.quote;
+    // current.answer = json.answer;
+
+    // let quoteItem = json.data.quote;
+    // console.log(quoteItem);
+    // let quoteAuthor = json.data.author;
+    // console.log(quoteAuthor);
+
+    // let fullQuote = `${quoteItem} ${quoteAuthor}`;
+    // console.log(fullQuote);
+
+    // if (quoteItem.charAt(quoteItem.length - 1) === "@") {
+    //     console.log("Present @ character found. Removing now.");
+    //   quoteItem = quoteItem.substring(0, quoteItem.length - 2);
+    // }
+
+    displayIcon();
+    // displayQuote(quoteItem, quoteAuthor);
+  } catch (err) {
+    console.log(err);
+    alert("FAIL ICON");
+  }
+}
+
+function displayQuote(quote, author) {
+  console.log("displayQuote called");
+  const quoteText = document.querySelector("#js-quote-text");
+  if (quote.charAt(quote.length - 1) === "@") {
+    console.log("Present @ character found.");
+    quoteText.textContent = `${quote + author}`;
+  } else if (author === "") {
+    console.log("Unkonwn or blank author.");
+    author = "";
+    quoteText.textContent = `${quote} ~Unknown`;
+  } else {
+    quoteText.textContent = `${quote} ~${author}`;
+  }
+  //   answerBtn.addEventListener("click", () => {
+  //     answerText.textContent = curr.answer;
+  //   });
+}
+
+function displayIcon() {
+  console.log("displayIcon called");
 }
 
 getQuote();
+getIcon();
