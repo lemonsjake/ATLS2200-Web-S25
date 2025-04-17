41de2ad5799f3258f28a2b68882879134a17af42
Lemons <jacob.lemons@colorado.edu>
2025-04-17 15:16:51 -0600

    in-progress wa13

---
+++ wa/wa13/wa13.js
@@ -0,0 +1,54 @@
+let company = {
+  companyName: "Tech Stars",
+  website: "www.techstars.site",
+  employees: [
+    {
+      firstName: "Sam",
+      department: "Tech",
+      designation: "Manager",
+      salary: 40000,
+      raiseEligible: true
+    },
+    {
+      firstName: "Mary",
+      department: "Finance",
+      designation: "Trainee",
+      salary: 18500,
+      raiseEligible: true
+    },
+    {
+      firstName: "Bill",
+      department: "HR",
+      designation: "Executive",
+      salary: 21200,
+      raiseEligible: false
+    },
+  ]
+};
+
+
+// PROBLEM 1
+console.log("PROBLEM 1");
+console.log(company.employees[0], company.employees[1], company.employees[2]);
+
+
+// PROBLEM 2
+console.log("\nPROBLEM 2");
+console.log(company);
+
+
+// PROBLEM 3
+console.log("\nPROBLEM 3");
+
+
+
+// PROBLEM 4
+console.log("\nPROBLEM 4");
+
+
+// PROBLEM 5
+console.log("\nPROBLEM 5");
+
+
+// PROBLEM 6
+console.log("\nPROBLEM 6");
