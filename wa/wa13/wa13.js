7087cb23b22d13f406e18c71dc8cd233afa9ef22
Lemons <jacob.lemons@colorado.edu>
2025-04-18 21:28:49 -0600

    typo fix wa13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -7,35 +7,32 @@ let company = {
       department: "Tech",
       designation: "Manager",
       salary: 40000,
-      raiseEligible: true
+      raiseEligible: true,
     },
     {
       firstName: "Mary",
       department: "Finance",
       designation: "Trainee",
       salary: 18500,
-      raiseEligible: true
+      raiseEligible: true,
     },
     {
       firstName: "Bill",
       department: "HR",
       designation: "Executive",
       salary: 21200,
-      raiseEligible: false
+      raiseEligible: false,
     },
-  ]
+  ],
 };
-
 
 // PROBLEM 1
 console.log("PROBLEM 1: Three employees");
 console.log(company.employees[0], company.employees[1], company.employees[2]);
 
-
 // PROBLEM 2
 console.log("\nPROBLEM 2: Company info");
 console.log(company);
-
 
 // PROBLEM 3
 console.log("\nPROBLEM 3: New employee, Anna");
@@ -44,12 +41,11 @@ company.employees.push({
   department: "Tech",
   designation: "Executive",
   salary: 25600,
-  raiseEligible: false
+  raiseEligible: false,
 });
 console.log(company);
 // console.log(company.employees.length);
 console.log(company.employees[3]);
-
 
 // PROBLEM 4
 console.log("\n--PROBLEM 4: Total salary of four employees");
@@ -57,9 +53,8 @@ let totalSalary = 0;
 for (employee of company.employees) {
   totalSalary += employee.salary;
   console.log(employee.firstName, employee.salary, employee);
-};
+}
 console.log(totalSalary);
-
 
 // PROBLEM 5
 console.log("\n--PROBLEM 5: Raising salary");
@@ -88,11 +83,12 @@ for (employee of company.employees) {
 }
 // console.log(company.employees);
 
-
 // PROBLEM 6
 console.log("\n--PROBLEM 6: Work from home (wfh) employees");
 for (employee of company.employees) {
-  employee.firstName === "Anna" || employee.firstName === "Sam" ? employee.wfh = true : employee.wfh = false;
+  employee.firstName === "Anna" || employee.firstName === "Sam"
+    ? (employee.wfh = true)
+    : (employee.wfh = false);
   // if ((employee.firstName === "Anna") || (employee.firstName === "Sam")) {
   //   employee.wfh = true;
   // }
