ccd3c7c26164aee6cb99b2efedf5934afce3bfac
Lemons <jacob.lemons@colorado.edu>
2025-04-18 21:22:50 -0600

    Completed wa13
    Finished weekly assignment 13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -28,27 +28,77 @@ let company = {
 
 
 // PROBLEM 1
-console.log("PROBLEM 1");
+console.log("PROBLEM 1: Three employees");
 console.log(company.employees[0], company.employees[1], company.employees[2]);
 
 
 // PROBLEM 2
-console.log("\nPROBLEM 2");
+console.log("\nPROBLEM 2: Company info");
 console.log(company);
 
 
 // PROBLEM 3
-console.log("\nPROBLEM 3");
-
+console.log("\nPROBLEM 3: New employee, Anna");
+company.employees.push({
+  firstName: "Anna",
+  department: "Tech",
+  designation: "Executive",
+  salary: 25600,
+  raiseEligible: false
+});
+console.log(company);
+// console.log(company.employees.length);
+console.log(company.employees[3]);
 
 
 // PROBLEM 4
-console.log("\nPROBLEM 4");
+console.log("\n--PROBLEM 4: Total salary of four employees");
+let totalSalary = 0;
+for (employee of company.employees) {
+  totalSalary += employee.salary;
+  console.log(employee.firstName, employee.salary, employee);
+};
+console.log(totalSalary);
 
 
 // PROBLEM 5
-console.log("\nPROBLEM 5");
+console.log("\n--PROBLEM 5: Raising salary");
+function raiseSalary(employee) {
+  if (employee.raiseEligible) {
+    employee.salary *= 1.1;
+    employee.raiseEligible = false;
+  }
+}
+for (employee of company.employees) {
+  console.log(employee);
+  if (employee.raiseEligible) {
+    // let empStr = employee.firstName + " " + employee.salary + " " + employee.raiseEligible + " --> ";
+    // raiseSalary(employee);
+    // empStr += employee.salary + " " + employee.raiseEligible;
+    // console.log(empStr);
+    raiseSalary(employee);
+    console.log("Raised", employee);
+  }
+  // else {
+  //   console.log(employee.firstName, employee.salary, employee.raiseEligible);
+  // }
+  // console.log(employee.firstName, employee.salary, employee.raiseEligible);
+  // raiseSalary(employee);
+  // console.log(employee.firstName, employee.salary, employee.raiseEligible);
+}
+// console.log(company.employees);
 
 
 // PROBLEM 6
-console.log("\nPROBLEM 6");
+console.log("\n--PROBLEM 6 -- Work from home (wfh) employees");
+for (employee of company.employees) {
+  employee.firstName === "Anna" || employee.firstName === "Sam" ? employee.wfh = true : employee.wfh = false;
+  // if ((employee.firstName === "Anna") || (employee.firstName === "Sam")) {
+  //   employee.wfh = true;
+  // }
+  // else {
+  //   employee.wfh = false;
+  // }
+  console.log(employee.firstName, employee.wfh, employee);
+}
+// console.log(company.employees);
